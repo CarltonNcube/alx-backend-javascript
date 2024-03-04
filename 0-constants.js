b096a92b7892f93ca2d3492cd3a322fc6f7c121f
CarltonNcube <carltonncube@outlook.com>
2024-03-04 19:07:28 +0000

    Const or let

---
+++ 0-constants.js
@@ -0,0 +1,7 @@
+// 0-constants.js
+
+export const taskFirst = () => 'I prefer const when I can.';
+
+export const getLast = () => ' is okay';
+
+export const taskNext = () => `But sometimes let${getLast()}`;
