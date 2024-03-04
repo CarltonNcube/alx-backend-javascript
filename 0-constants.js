9bf3a31dc4f9b00f6c19ec88d4eb250bdaaaa1bf
CarltonNcube <carltonncube@outlook.com>
2024-03-04 19:25:03 +0000

    Const or let

--- 0-constants.js
+++ 0-constants.js
@@ -1,7 +1,16 @@
 // 0-constants.js
 
-export const taskFirst = () => 'I prefer const when I can.';
-
-export const getLast = () => ' is okay';
-
-export const taskNext = () => `But sometimes let${getLast()}`;
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+    return combination;
+  }
