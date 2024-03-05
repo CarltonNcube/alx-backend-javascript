8c5ac5efe1782bf87d2a258f89f8ff34b4b135bf
CarltonNcube <carltonncube@outlook.com>
2024-03-05 21:50:55 +0000

    a function named guardrail that will accept one argument mathFunction (Function)

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,13 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  
+
   try {
     queue.push(mathFunction());
-  } catch (error) {
-    queue.push(error.message);
+  } catch (err) {
+    queue.push(`${err}`);
   } finally {
     queue.push('Guardrail was processed');
   }
-  
+
   return queue;
 }
