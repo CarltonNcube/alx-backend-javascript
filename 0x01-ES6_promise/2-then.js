af49dee33f107634b6695444ec0ef25c765e8cc2
CarltonNcube <carltonncube@outlook.com>
2024-03-05 20:40:39 +0000

    the Promise resolves, return an object with the following attributes status: 200 body: succes

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,10 @@
 function handleResponseFromAPI(promise) {
-  promise.then(
-    // On resolve
-    () => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    },
-    // On reject
-    () => {
-      console.log('Got a response from the API');
-      return new Error();
-    }
-  );
+  promise
+    .then((result) => {
+      console.log("Got a response from the API");
+      return { status: 200, body: "success" };
+    })
+    .catch(() => new Error());
 }
+
+export default handleResponseFromAPI;
