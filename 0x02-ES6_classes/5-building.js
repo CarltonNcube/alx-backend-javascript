f0894a75ab43ef1dec91c44266c367af2ea99560
CarltonNcube <carltonncube@outlook.com>
2024-03-07 21:48:49 +0000

    Implement a class named Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,12 @@
 class Building {
     constructor(sqft) {
-        if (new.target === Building) {
+        this._sqft = sqft;
+        if (this.constructor === Building) {
             throw new Error("Cannot instantiate abstract class Building");
         }
-
-        this._sqft = sqft;
+        if (typeof this.evacuationWarningMessage !== "function") {
+            throw new Error("Class extending Building must override evacuationWarningMessage");
+        }
     }
 
     get sqft() {
@@ -13,10 +15,6 @@ class Building {
 
     set sqft(value) {
         this._sqft = value;
-    }
-
-    evacuationWarningMessage() {
-        throw new Error("Class extending Building must override evacuationWarningMessage");
     }
 }
 
