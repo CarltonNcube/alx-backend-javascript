bd80677903bc3467d28dec3f6fb6ca85663817dc
CarltonNcube <carltonncube@outlook.com>
2024-03-07 20:10:49 +0000

    Implement a class named Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+//Define an abstract class named building
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
