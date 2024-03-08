e997444db1ffb786a92004852727562740f93484
CarltonNcube <carltonncube@outlook.com>
2024-03-08 17:07:03 +0000

    Implement a class named Car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,23 +1,24 @@
+// Represents a Car class with a method for cloning objects.
 const cloneCarSymbol = Symbol('cloneCar');
 
 class Car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    [cloneCarSymbol]() {
-        const { _brand, _motor, _color } = this;
-        return new Car(_brand, _motor, _color);
-    }
+  [cloneCarSymbol]() {
+    const { _brand, _motor, _color } = this;
+    return new Car(_brand, _motor, _color);
+  }
 
-    cloneCar() {
-        if (!(this instanceof Car)) {
-            throw new Error('Species check failed: Object must be an instance of Car class');
-        }
-        return this[cloneCarSymbol]();
+  cloneCar() {
+    if (!(this instanceof Car)) {
+      throw new Error('Species check failed: Object must be an instance of Car class');
     }
+    return this[cloneCarSymbol]();
+  }
 }
 
 export default Car;
