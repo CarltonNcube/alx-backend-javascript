7db8c60c0d4397cee502fe01f55bb3e4af399799
CarltonNcube <carltonncube@outlook.com>
2024-03-08 18:52:35 +0000

    a  function appendToEachArrayValue to use adds for...of operator

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
+// Creates an iterator object from the provided report object.
+export default function createIteratorObject(report) {
+  const result = [];
+  for (const idx of Object.values(report.allEmployees)) {
+    result.push(...idx);
   }
-
-  return array;
+  return result;
 }
