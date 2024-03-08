d1b0db536eaabb4d89bb02d16be2c5cc92fd60ab
CarltonNcube <carltonncube@outlook.com>
2024-03-08 17:22:01 +0000

    Implement a class named EVCar that extends the Car class:

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,19 +1,13 @@
 // Import Car from 10-car.js
 import Car from './10-car.js';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  get range() {
-    return this._range;
-  }
-
-  cloneCar() {
-    return super.cloneCar();
+  static get [Symbol.species]() {
+    return Car;
   }
 }
-
-export default EVCar;
