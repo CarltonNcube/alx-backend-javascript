70d74c41800bad94dc6d8ac64135c5f7f0f31060
CarltonNcube <carltonncube@outlook.com>
2024-03-11 18:34:56 +0000

    a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,14 @@
+// Function that updates student grades by city
+const updateStudentGradeByCity = (students, city, newGrades) => {
+    return students
+        .filter(student => student.location === city)
+        .map(student => {
+            const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+            return {
+                ...student,
+                grade: gradeObj ? gradeObj.grade : 'N/A'
+            };
+        });
+};
+
+export default updateStudentGradeByCity;
