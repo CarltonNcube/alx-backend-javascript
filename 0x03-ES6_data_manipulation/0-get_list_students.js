e32d93e4da0100dabe79a3545452835115befedd
CarltonNcube <carltonncube@outlook.com>
2024-03-11 17:39:50 +0000

    a function named getListStudents that returns an array of objects.

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,20 @@
 // Function that returns an array of objects representing students
 const getListStudents = () => [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
 ];
 
 export default getListStudents;
