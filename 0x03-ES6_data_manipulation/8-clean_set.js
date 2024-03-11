583e67ebacc7f0c9e1bb2d340c551001786c1f82
CarltonNcube <carltonncube@outlook.com>
2024-03-11 19:18:23 +0000

    a function named cleanSet that returns a string of all the set values that start with a specific string (startString).

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,14 @@
 // Function that returns a string of set values starting with a specific string
-function cleanSet(set, startString) {
-  let result = '';
-  set.forEach(value => {
-    if (value.startsWith(startString)) {
-      result += value.slice(startString.length) + '-';
+const cleanSet = (set, startString) => {
+  const strings = [];
+
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      strings.push(s.slice(startString.length));
     }
   });
-  return result.slice(0, -1);
-}
+  return strings.join('-');
+};
 
 export default cleanSet;
