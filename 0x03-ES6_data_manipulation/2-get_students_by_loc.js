8a33bc90332b0ac264815f9154251a81eb76d113
CarltonNcube <carltonncube@outlook.com>
2024-03-11 18:08:47 +0000

    a function getStudentsByLocation that returns an array of objects who are located in a specific city

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+// Function that filters students by location
+const getStudentsByLocation = (students, city) => {
+    return students.filter(student => student.location === city);
+};
+
+export default getStudentsByLocation;
