d2a852dcec7095a8c95d849f1949d1b3138e3c01
CarltonNcube <carltonncube@outlook.com>
2024-03-11 17:51:13 +0000

    a function getListStudentIds that returns an array of ids from a list of object

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+// Function that returns an array of student ids from a list of objects
+
+const getListStudentIds = (students) => {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+    return students.map(student => student.id);
+}
+
+export default getListStudentIds;
