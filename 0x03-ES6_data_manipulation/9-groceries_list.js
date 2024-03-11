44dff5ca10a6e8ac044b644110af364fb1a32c16
CarltonNcube <carltonncube@outlook.com>
2024-03-11 19:37:38 +0000

    a function named groceriesList that returns a map of groceries with the following items (name, quantity)

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,13 +1,12 @@
 // Function that returns a map of groceries with their quantities
-function groceriesList() {
-  const groceries = new Map([
-    ['Apples', 10],
-    ['Tomatoes', 10],
-    ['Pasta', 1],
-    ['Rice', 1],
-    ['Banana', 5]
-  ]);
-  return groceries;
-}
+const groceriesList = () => {
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
+  return map;
+};
 
 export default groceriesList;
