7ed9e43dc635398cba39cb4ad650ab48231cdd18
Carlton Ncube <carltonncube@outlook.com>
2024-04-30 23:57:20 +0200

    a function countStudents in the file 2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function countStudents(path) {
+    let data;
+    try {
+        data = fs.readFileSync(path, 'utf8').split('\n');
+    } catch (err) {
+        throw new Error('Cannot load the database');
+    }
+
+    data = data.filter((line) => line.length > 0 && !line.includes('firstname,lastname,age,field'));
+    console.log(`Number of students: ${data.length}`);
+
+    const fields = {};
+    for (const line of data) {
+        const field = line.split(',')[3];
+        if (!fields[field]) {
+            fields[field] = [];
+        }
+        fields[field].push(line.split(',')[0]);
+    }
+
+    for (const field in fields) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+}
+
+module.exports = countStudents;
+
