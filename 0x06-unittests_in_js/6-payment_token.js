3b4410423c353d051034d7936b7604707db58dc9
Carlton Ncube <carltonncube@outlook.com>
2024-05-02 23:35:15 +0200

    a new function named getPaymentTokenFromAPI that will take an argument called success (boolean)

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,9 +1,9 @@
 function getPaymentTokenFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
+  if (success) {
+    return new Promise((resolve, reject) => {
       resolve({ data: 'Successful response from the API' });
-    }
-  });
+    });
+  }
 }
 
 module.exports = getPaymentTokenFromAPI;
