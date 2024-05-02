32dc7955aad095f710f281226f5efdf5c1a1b538
Carlton Ncube <carltonncube@outlook.com>
2024-05-03 00:06:25 +0200

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,34 +1,28 @@
-const request = require('request');
-const expect = require('chai').expect;
-const app = require('./api.js');
+const request = require("request");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-describe('getPaymentTokenFromAPI', () => {
-  it('should return correct result for index page', (done) => {
-    request('http://localhost:7865', (error, response, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
     });
-  });
-
-  it('should return correct result for cart route', (done) => {
-    request('http://localhost:7865/cart/12', (error, response, body) => {
-      expect(body).to.equal('Payment methods for cart 12');
-      done();
+    it("check correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
     });
-  });
-
-  it('should return correct result for different cart id', (done) => {
-    request('http://localhost:7865/cart/99', (error, response, body) => {
-      expect(body).to.equal('Payment methods for cart 99');
-      done();
+    it("check correct content length", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.headers['content-length']).to.equal('29');
+	    done();
+	});
     });
-  });
-
-  it('should return status code 200 for cart route', (done) => {
-    request('http://localhost:7865/cart/12', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
 });
-
