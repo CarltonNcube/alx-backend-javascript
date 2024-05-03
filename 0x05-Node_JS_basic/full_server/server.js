1da210b5c9b8eadb281ff8fc65b67ab8053a90f6
Carlton Ncube <carltonncube@outlook.com>
2024-05-03 23:20:59 +0200

    Organizesing complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,12 +1,13 @@
 // full_server/server.js
 import express from 'express';
-import router from './routes';
+import router from './routes/index.js';
 
 const app = express();
-app.use('/', router);
+app.use(router);
 
-app.listen(1245, () => {
-  console.log('Server running on port 1245');
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 export default app;
